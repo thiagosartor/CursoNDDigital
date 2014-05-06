d801cc8c0d37ca5cef677690868f3e4ba7d7fb79
thiagosartor <tt.sartor@gmail.com>
2014-05-06 08:45:34 -0300

    Subindo o material para o servidor.

---
+++ CursoNDDigital/UNIDADE VI/Operadores.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace UNIDADE_VI
+{
+    internal class Operadores
+    {
+        public static void Main_()
+        {
+            Console.WriteLine("Pré-Incremento:\n");
+
+            int x = 0;
+
+            Console.WriteLine("x = " + x);
+
+            Console.WriteLine("\n++x +20 = \n");
+
+            Console.WriteLine(++x + 20 + "\n");
+
+            Console.WriteLine("\nPós-Incremento:\n");
+
+            x = 0;
+
+            Console.WriteLine("\nx++ +20 = \n");
+
+            Console.WriteLine(x++ + 20 + "\n");
+
+            Console.WriteLine("\nPré-Decremento:\n");
+
+            x = 0;
+
+            Console.WriteLine("x = " + x);
+
+            Console.WriteLine("\n--x +20 = \n");
+
+            Console.WriteLine(--x + 20 + "\n");
+
+            Console.WriteLine("\nPós-Decremento:\n");
+
+            x = 0;
+
+            Console.WriteLine("\nx-- +20 = \n");
+
+            Console.WriteLine(x-- + 20 + "\n");
+
+            Console.ReadKey();
+        }
+
+        public static void Main()
+        {
+
+        }
+    }
+}
