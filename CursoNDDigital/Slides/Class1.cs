95abb7fa48daa5d026be474d23e41a11d8e24ff7
thiagosartor <tt.sartor@hotmail.com>
2014-06-06 15:50:19 -0300

    .

---
+++ CursoNDDigital/Slides/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Slides
+{
+    internal class Class1
+    {
+        public static void Main()
+        {
+            int i = 0;
+
+            Random gerador = new Random();
+
+            // número no intervalo [0 ,99]
+            int numeroAleatorio = (int)(gerador.NextDouble() * 100);
+
+            while (i < numeroAleatorio)
+            {
+                Console.WriteLine(" Mensagem número " + (i + 1));
+                i++;
+            }
+        }
+    }
+}
